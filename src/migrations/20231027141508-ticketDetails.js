e4fc1e7a65558e598da07953dfd064b3a664c3d4
andrianggoro21 <andri.anggoro21@gmail.com>
2023-10-27 22:45:15 +0700

    update migrations

---
+++ src/migrations/20231027141508-ticketDetails.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable("ticketDetails", {
+      detailId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      typeId: {
+        type: Sequelize.INTEGER,
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+      },
+      capacity: {
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable("ticketDetails");
+  }
+};
